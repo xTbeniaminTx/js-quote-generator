e5d70467c8b48461377787a8d999c7c9c1608100
Beniamin Tolan thinkCenter <beniamin.tolan@outlook.com>
2021-02-18 15:03:29 +0100

    new quote and twiter button

--- script.js
+++ script.js
@@ -11,13 +11,30 @@ async function getQuote() {
         const response = await fetch(apiUrl);
         const data = await response.json();
         let number = Math.floor(Math.random() * data.length);
-
+        if (data[number].text.length > 120) {
+            quoteText.classList.add('long-text');
+        } else {
+            quoteText.classList.remove('long-text');
+        }
         authorText.innerText = data[number].author;
         quoteText.innerText = data[number].text;
+
     } catch (error) {
         console.log(error);
     }
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const tweeterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(tweeterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
